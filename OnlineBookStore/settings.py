b374e6153d96ccdcc11ea3a1c9b3fb214229b42e
UmutYvz <umutyvz.77@gmail.com>
2021-03-31 20:06:00 +0300

    product category model created

--- OnlineBookStore/settings.py
+++ OnlineBookStore/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'home.apps.HomeConfig',
+    'product.apps.ProductConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
