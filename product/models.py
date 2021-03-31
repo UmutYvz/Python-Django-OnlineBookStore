1aa11774af64c17be52af31be26dad70fc95c08f
UmutYvz <umutyvz.77@gmail.com>
2021-03-31 20:51:11 +0300

    Product table added and relationship added between category and product.

--- product/models.py
+++ product/models.py
@@ -21,3 +21,30 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Product(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)  # relation with category
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=255)
+    keywords = models.CharField(max_length=255)
+    price = models.FloatField()
+    amount = models.IntegerField()
+    detail = models.TextField()
+    author = models.CharField(max_length=50)
+    publisher = models.CharField(max_length=200)
+    pages = models.IntegerField()
+    year_released = models.IntegerField()
+    size = models.CharField(max_length=30)
+    image = models.ImageField(blank=True, upload_to='images/')
+    status = models.CharField(max_length=10, choices=STATUS)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_ad = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
