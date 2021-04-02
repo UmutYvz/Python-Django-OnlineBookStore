33a3b1b99dc9ebc6ef14e3d8cda54ccefc267eb8
UmutYvz <umutyvz.77@gmail.com>
2021-04-02 17:06:00 +0300

    Image gallery for product added for admin page.

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from product.models import Category, Product
+from product.models import Category, Product, Images
+
+
+class ProductImageInLine(admin.TabularInline):
+    model = Images
+    extra = 3
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -11,8 +16,14 @@ class CategoryAdmin(admin.ModelAdmin):
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'category', 'price', 'amount', 'author', 'status']
-    list_filter = ['status','category']
+    list_filter = ['status', 'category']
+    inlines = [ProductImageInLine]
+
+
+class ImagesAdmin(admin.ModelAdmin):
+    list_display = ['title', 'product', 'image']
 
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Images, ImagesAdmin)
